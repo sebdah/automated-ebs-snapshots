b113bef711fdaa368da05e724310aa3afb0e79f3
Sebastian Dahlgren <sebastian.dahlgren@skymill.se>
2014-01-27 09:08:26 +0100

    Moved the command line options to separate module

--- automated_ebs_snapshots/__init__.py
+++ automated_ebs_snapshots/__init__.py
@@ -1,12 +1,11 @@
 """ Automatic AWS EBS snapshot handling """
-import argparse
 import logging
 import logging.config
 
+from automated_ebs_snapshots.command_line_options import args
 from automated_ebs_snapshots import config_file_parser
 from automated_ebs_snapshots import connection_manager
 from automated_ebs_snapshots import volume_manager
-from automated_ebs_snapshots.valid_intervals import VALID_INTERVALS
 
 logging.config.dictConfig({
     'version': 1,
@@ -57,50 +56,6 @@ logger = logging.getLogger(__name__)
 
 def main():
     """ Main function """
-    parser = argparse.ArgumentParser(
-        description='Automatic AWS EBS snapshot handling')
-    aws_config_ag = parser.add_argument_group(
-        title='AWS configuration options')
-    aws_config_ag.add_argument(
-        '--access-key-id',
-        help='AWS access key')
-    aws_config_ag.add_argument(
-        '--secret-access-key',
-        help='AWS secret access key')
-    aws_config_ag.add_argument(
-        '--region',
-        default='us-east-1',
-        help='AWS region')
-    general_ag = parser.add_argument_group(
-        title='General')
-    general_ag.add_argument(
-        '-c', '--config',
-        help='Configuration file to read')
-    general_ag.add_argument(
-        '-i', '--interval',
-        default='daily',
-        help='Volume snapshotting interval. Valid values are: {}'.format(
-            ', '.join(VALID_INTERVALS)))
-    admin_actions_ag = parser.add_argument_group(
-        title='Administrative actions')
-    admin_actions_ag.add_argument(
-        '--list',
-        action='count',
-        help='List volumes that we are watching')
-    admin_actions_ag.add_argument(
-        '--unwatch',
-        metavar='VOLUME_ID',
-        help=(
-            'Remove an EBS volume from the watch list. '
-            'Usage: --unwatch vol-12345678'))
-    admin_actions_ag.add_argument(
-        '--watch',
-        metavar='VOLUME_ID',
-        help=(
-            'Add a new EBS volume to the watch list. '
-            'Usage: --watch vol-12345678'))
-    args = parser.parse_args()
-
     # Read configuration from the config file if present, else fall back to
     # command line options
     if args.config:
