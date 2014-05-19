0b9d976b778bc08005f6e552bc499b50c4bb073b
Sebastian Dahlgren <sebastian.dahlgren@skymill.se>
2014-05-19 10:59:35 +0200

    Fixed instance profile authentication #5

--- automated_ebs_snapshots/connection_manager.py
+++ automated_ebs_snapshots/connection_manager.py
@@ -3,6 +3,7 @@ import logging
 import sys
 
 from boto import ec2
+from boto.utils import get_instance_metadata
 
 logger = logging.getLogger(__name__)
 
@@ -19,12 +20,27 @@ def connect_to_ec2(region='us-east-1', access_key=None, secret_key=None):
     :returns: boto.ec2.connection.EC2Connection -- EC2 connection
     """
     logger.info('Connecting to AWS EC2 in {}'.format(region))
+
+    # Fetch instance metadata
+    metadata = get_instance_metadata(timeout=1, num_retries=1)
+    if metadata:
+        try:
+            profile_name = metadata['iam']['info'][u'InstanceProfileArn']
+            region = metadata['placement']['availability-zone'][:-1]
+        except KeyError:
+            profile_name = None
+
     if access_key:
+        # Connect using supplied credentials
         connection = ec2.connect_to_region(
             region,
             aws_access_key_id=access_key,
             aws_secret_access_key=secret_key)
+    elif metadata:
+        # Connect using instance profile
+        connection = ec2.connect_to_region(region, profile_name=profile_name)
     else:
+        # Connect using env vars or boto credentials
         connection = ec2.connect_to_region(region)
 
     if not connection:
